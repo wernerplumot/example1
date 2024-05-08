ec581b0dc846ded0934e09770072db33d147f449
WP <wplumot@gmail.com>
2024-05-09 01:12:11 +0200

    add hello

--- lib/Example.ex
+++ lib/Example.ex
@@ -2,7 +2,11 @@ defmodule Example do
   use Application
 
   def start(_type, _args) do
-    IO.puts("Hello world")
+    IO.puts(Example.hello())
     Supervisor.start_link([], strategy: :one_for_one)
+  end
+
+  def hello do
+    "Hello World"
   end
 end
